dfc938bba87dea44e5906271e4555d431c0c3934
DeiseClaudino <deise.claudino98@gmail.com>
2020-05-19 22:02:06 -0300

    Trabalha com array de inteiros

---
+++ listas.php
@@ -0,0 +1,10 @@
+<?php
+
+$idadeList = [21, 23, 19, 25, 30, 41, 18];
+// pega primeiro item do array
+$primeiraIdade = $idadeList[0];
+
+// pega terceiro item
+$terceiraIdade = $idadeList[2];
+
+echo "$terceiraIdade";
