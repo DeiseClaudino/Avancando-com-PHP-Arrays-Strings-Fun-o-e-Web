f4b40b0fa78625b551072af819bbd5cbd3bcde4b
DeiseClaudino <deise.claudino98@gmail.com>
2020-06-06 22:13:29 -0300

    Refatorando para um método

--- index.php
+++ index.php
@@ -13,13 +13,6 @@ $notas = [
 $calculadora = new Calculadora();
 $media = $calculadora->calculaMedia($notas);
 
-echo "<p>a nota de português é:  $notas[0]</p>";
-echo "<p>a nota de matemática é: $notas[1]</p>";
-echo "<p>a nota de geografia é:  $notas[2]</p>";
-echo "<p>a nota de história é:   $notas[3]</p>";
-echo "<p>a nota de quimica é:    $notas[4]</p>";
-echo "<p>a nota de artes é:      $notas[5]</p>";
-
 if($media)
 {
     echo "A média é: $media";
