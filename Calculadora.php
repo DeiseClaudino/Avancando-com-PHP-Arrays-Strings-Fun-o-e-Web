de4ce3055d31b1fd9620c3f571a84b5ff2de47eb
DeiseClaudino <deise.claudino98@gmail.com>
2020-06-06 21:53:58 -0300

    Refatorando para um método

---
+++ Calculadora.php
@@ -0,0 +1,27 @@
+<?php
+
+class Calculadora
+{
+    public function calculaMedia(array $notas): ?float
+    {
+        $quantidadeNotas = sizeof($notas);
+        $soma = 0;
+
+        if($quantidadeNotas > 0)
+        {
+            for($i = 0; $i < $quantidadeNotas; $i++)
+            {
+                $soma = $soma + $notas[$i]; 
+            }
+
+            $media = $soma / $quantidadeNotas;
+
+            return $media;
+        }
+        else
+        {
+            return null;
+        }
+
+    }    
+}
--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
+require 'Calculadora.php';
 
-$notaPortugues  = 9;
-$notaMatematica = 3;
-$notaGeografia  = 10;
-$notaHistoria   = 5; 
-$notaQuimica    = 10;
+$notas = [
+    9,
+    3,
+    10,
+    5,
+    10,
+    8
+];
 
-$media = ($notaPortugues + $notaMatematica + $notaGeografia + $notaHistoria + $notaQuimica) / 5;
+$calculadora = new Calculadora();
+$media = $calculadora->calculaMedia($notas);
 
-echo "<p>a nota de português é:  $notaPortugues</p>";
-echo "<p>a nota de matemática é:  $notaMatematica</p>";
-echo "<p>a nota de geografia é:  $notaGeografia</p>";
-echo "<p>a nota de história é:  $notaHistoria</p>";
-echo "<p>a nota de quimica é:  $notaQuimica</p>";
-echo "<p>A média é: $media</p>";
+echo "<p>a nota de português é:  $notas[0]</p>";
+echo "<p>a nota de matemática é: $notas[1]</p>";
+echo "<p>a nota de geografia é:  $notas[2]</p>";
+echo "<p>a nota de história é:   $notas[3]</p>";
+echo "<p>a nota de quimica é:    $notas[4]</p>";
+echo "<p>a nota de artes é:      $notas[5]</p>";
 
-
+if($media)
+{
+    echo "A média é: $media";
+}
+else
+{
+    echo "Não foi possível calcular a média";
+}
