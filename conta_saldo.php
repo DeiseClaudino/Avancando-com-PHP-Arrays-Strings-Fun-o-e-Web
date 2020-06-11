8d489cf5a6f75947d2090f638fcc08495ce11fcb
DeiseClaudino <deise.claudino98@gmail.com>
2020-06-11 12:22:30 -0300

    Operações com arrays associativos

--- conta_saldo.php
+++ conta_saldo.php
@@ -27,6 +27,17 @@ $relacionados = array_combine($correntistas, $saldos);
 
 $relacionados['Matheus'] = 4500;
 
-echo "<pre>";
-var_dump ($relacionados);
-echo "</pre>";
+// echo "<pre>";
+// var_dump ($relacionados);
+// echo "</pre>";
+
+// echo "O saldo de Giovanni é: {$relacionados['Giovanni']}";
+
+if(array_key_exists('João', $relacionados))
+{
+    echo "O saldo de João é: {$relacionados['João']}";
+}
+else
+{
+    echo "Não foi encontrado";
+}
